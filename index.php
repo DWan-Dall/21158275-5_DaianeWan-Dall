234ac7a8695facbf51ca9278b619f87e8addb8f4
DWan-Dall <dwandall@gmail.com>
2021-08-27 10:08:37 -0300

    Criação da página index

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The_Cook_Store</title>
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="logo.JPG" style="width: 150px; height: 80px;">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-4 mb-md-4">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<main>
+    <div class="container marketing">
+
+
+
+        <hr class="featurette-divider">
+    </div>
+
+    <!-- FOOTER -->
+    <p class="float-end"><a href="#">Voltar ao topo</a></p>
+    <p class="text-center">&copy; 2021 - Daiane Wan-Dall Splitter da Silva - RA: 21158275-5</p>
+    </footer>
+</main>
+
+<script src="/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
